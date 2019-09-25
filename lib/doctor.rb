57db545b5b6ba56ea4225431af2b8849d7e6a9b5
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-25 17:25:11 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -26,6 +26,22 @@ class Doctor
     Appointment.all.select {|appointment|appointment.doctor == self}
   end
     
+      def new_song(name,genre)
+    Song.new(name,self,genre)
+  end
+  
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def genres
+    songs.collect {|song| song.genre}
+  end
+  
+    
+    
+    
+    
 
   
 end
