919e8fca8805e7427f3855828c2261e2627d7a3c
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-25 18:16:03 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -14,7 +14,7 @@ class Patient
   end
   
   def appointments
-    Appointment.all.select {|appointment|appointment == self}
+    Appointment.all.select {|appointment|appointment.patient == self}
   end
   
   def new_appointment(date,doctor)
