2cb7844473ad4c949d101924a10766a5e7885a9b
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-25 17:15:20 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -2,8 +2,29 @@ class Appointment
   
   @@all = []
   
-  attr_accessor :appointment 
+  attr_accessor :name, patient, doctor 
   def initilaize()
+  
+  
+
+  
+  attr_accessor :name, :genre, :artist
+  
+  def initialize(name, artist, genre)
+  @name = name
+  @@all << self
+  @genre = genre
+  @artist = artist
+  end
+
+  def self.all
+  @@all
+  end
+
+
+
+
+end
   
   
 end
