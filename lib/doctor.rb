70d5370cc95522c6df3e2faf688caa3dbb64cf80
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-25 18:14:18 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -19,7 +19,7 @@ class Doctor
   end
     
   def appointments
-    Appointment.all.select { |appointment| appointment.doctor == self}
+    Appointment.all.select {|meet|meet.doctor == self}
   end
 
   
